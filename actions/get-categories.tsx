87af9a6d069dbf450a21908455929cac769e55ee
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-05-27 15:23:02 +0545

    creating the store.

---
+++ actions/get-categories.tsx
@@ -0,0 +1,11 @@
+import { Category } from "@/type";
+
+const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
+
+const getCategories= async(): Promise<Category[]>=>{
+    const response=await fetch(URL);
+
+    return response.json();
+}
+
+export default getCategories;
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+  return (
+    <div>Hello store</div>
+  )
+}
+
+export default HomePage
